36d44978587b7691553e72995af37619021586f7
Juan cruz Leone <juancruzleone@outlook.com>
2025-06-28 21:36:15 -0300

    fix error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    target: "esnext",
+    minify: "esbuild",
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
 })
