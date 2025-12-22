abdd7cf74cae285b31a41ad74462a370aaa17b92
Juan cruz Leone <juancruzleone@outlook.com>
2025-12-22 15:07:43 -0300

    Feat: Modify design sidebar

--- src/shared/components/Nav/Nav.tsx
+++ src/shared/components/Nav/Nav.tsx
@@ -247,7 +247,7 @@ const Nav = () => {
                   onClick={toggleTheme}
                   aria-label={t('nav.toggleTheme')}
                 >
-                  {dark ? <Sun size={15} /> : <Moon size={15} />}
+                  {dark ? <Sun size={20} /> : <Moon size={20} />}
                 </button>
               </div>
             </div>
