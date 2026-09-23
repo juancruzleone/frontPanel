58fe4f3ef010b673bc3ef960348b5582f8173149
Juan Cruz Leone <juancruzleone@outlook.com>
2026-09-23 21:57:23 +0000

    revert: vuelve ModalAssignTemplate a estado original (botones dentro de assignFormInner)

--- src/features/assets/components/ModalAssignTemplate.tsx
+++ src/features/assets/components/ModalAssignTemplate.tsx
@@ -140,25 +140,26 @@ const ModalAssignTemplate = ({
                   {formErrors.general}
                 </div>
               )}
-            </div>
-            <div className={formButtonStyles.actions}>
-              <button
-                type="button"
-                onClick={handleClose}
-                disabled={isSubmitting}
-                className={formButtonStyles.cancelButton}
-              >
-                {t('common.cancel')}
-              </button>
-              <button
-                type="submit"
-                disabled={loadingTemplates || isSubmitting}
-                className={formButtonStyles.submitButton}
-                aria-label={t('assets.editTemplateTooltip')}
-                data-tooltip={t('assets.editTemplateTooltip')}
-              >
-                {isSubmitting ? t('common.saving') : t('assets.edit')}
-              </button>
+
+              <div className={formButtonStyles.actions}>
+                <button
+                  type="button"
+                  onClick={handleClose}
+                  disabled={isSubmitting}
+                  className={formButtonStyles.cancelButton}
+                >
+                  {t('common.cancel')}
+                </button>
+                <button
+                  type="submit"
+                  disabled={loadingTemplates || isSubmitting}
+                  className={formButtonStyles.submitButton}
+                  aria-label={t('assets.editTemplateTooltip')}
+                  data-tooltip={t('assets.editTemplateTooltip')}
+                >
+                  {isSubmitting ? t('common.saving') : t('assets.edit')}
+                </button>
+              </div>
             </div>
           </form>
         </div>
